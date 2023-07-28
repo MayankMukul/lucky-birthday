602555af9b0e7ea419b0b090ebcfa7d58408700a
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-07-29 00:13:58 +0530

    final

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 var btn = document.querySelector("#btn-lucky");
 var dob = document.querySelector("#birth-date");
 var luckynumber = document.querySelector("#lucky-number");
+var message = document.querySelector("#message");
 var dobsum ;
+
 function checkluckynumber(){
     console.log(dob.value+ " "+luckynumber.value);
     dobsum = dob.value.replaceAll('-','');
-    //dobsum = dobsum.replaceAll('','+');
     console.log(dobsum);
     let sum =0; 
     for(let i=0 ; i<8;i++) {
-        //dobsum[i]=dobsum[i]*1;   
-        sum = sum*1+dobsum[i]*1;
-       
+        sum = sum+dobsum[i]*1;
     }
     console.log(sum);
+    checklucky(sum,luckynumber.value);
+}
+
+function checklucky(sum,number){
+    
+    if (sum%number==0){
+        message.innerText=`${number} is lucky 🤩🥳🥳`;
+    }else {
+        message.innerText=(`${number} is Not that Lucky😥🥺🥺`);  
+    }
 }
 
 btn.addEventListener("click",checkluckynumber);
